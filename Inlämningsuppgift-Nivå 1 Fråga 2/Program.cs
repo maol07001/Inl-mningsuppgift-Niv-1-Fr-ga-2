a80ed8b189de3486d8c3bc327c2fcc51a19ea013
mariu <mariu@DESKTOP-CT70FK7>
2024-12-16 15:51:37 +0100

    Add project files.

---
+++ Inlämningsuppgift-Nivå 1 Fråga 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace uppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int nummer;
+
+            do
+            {
+                Console.Write("Skriv in ett heltal: ");
+                nummer = int.Parse(Console.ReadLine());
+
+                if (nummer != 0)
+                {
+                    Console.WriteLine("Testa igen");
+                }
+
+            } while (nummer != 0);
+
+            Console.WriteLine("Programmet avslutas. Tack!");
+        }
+    }
+}
